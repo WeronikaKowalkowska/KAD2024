d052ad0efce1575f6a1e79c6e5cd8086574afa54
Nadzeya Silchankava <nadezhdasilchenkova@gmail.com>
2024-11-02 10:26:30 +0100

    the first table - Liczności poszczególnych gatunków with round

--- Zadanie 1/main.py
+++ Zadanie 1/main.py
@@ -1,6 +1,3 @@
-from operator import index
-
-import numpy as np
 import pandas as pd
 
 
@@ -8,7 +5,6 @@ dane = pd.read_csv('dane.csv',
                    header = None,
                    names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Species'])
 
-#print(dane)
 
 #Liczności poszczególnych gatunków
 
@@ -17,11 +13,11 @@ gatunki_liczebność.index = ['Setosa', 'Versicolor', 'Virginica']
 gatunki_liczebność.loc['Razem'] = gatunki_liczebność.sum()
 gatunki_liczebność.name = 'Liczebność'
 
-
 liczności = pd.DataFrame()
 liczności['Liczebność'] = gatunki_liczebność
 liczności['Udział Procentowy (%)'] = gatunki_liczebność / gatunki_liczebność[0:3].sum() * 100
 liczności.index.name = 'Gatunek'
 
+pd.set_option('display.float_format', '{:.1f}'.format)
 print('Liczności poszczególnych gatunków:')
 print(liczności)
