303c181368a4cf870ff5108fd02fde7efe45bcc9
Nadzeya Silchankava <nadezhdasilchenkova@gmail.com>
2024-11-04 12:56:42 +0100

    connected

--- Zadanie 1/main.py
+++ Zadanie 1/main.py
@@ -6,13 +6,36 @@ dane = pd.read_csv('dane.csv',
                    header = None,
                    names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Species'])
 
+dlKielicha = np.array(dane['Sepal length'])
+szerKielicha = np.array(dane['Sepal width'])
+dlPlatka = np.array(dane['Petal length'])
+szerPlatka = np.array(dane['Petal width'])
+gatunek = np.array(dane['Species'])
+
+liczebnoscSet=0
+liczebnoscVer=0
+liczebnoscVir=0
+
+for i in range(0,len(gatunek)):
+    if gatunek[i] == 0:
+        liczebnoscSet += 1
+    elif gatunek[i] == 1:
+        liczebnoscVer += 1
+    else:
+        liczebnoscVir += 1
+
+
+liczebnoscCala=liczebnoscSet+liczebnoscVer+liczebnoscVir
+
 
 # Liczności poszczególnych gatunków
 
-gatunki_liczebność = (dane['Species']).value_counts()
-gatunki_liczebność.index = ['Setosa', 'Versicolor', 'Virginica']
-gatunki_liczebność.loc['Razem'] = gatunki_liczebność.sum()
-gatunki_liczebność.name = 'Liczebność'
+
+gatunki_liczebność = pd.DataFrame(columns=['Liczebność'])
+gatunki_liczebność.loc['Setosa'] = liczebnoscSet
+gatunki_liczebność.loc['Versicolor'] = liczebnoscVer
+gatunki_liczebność.loc['Virginica'] =liczebnoscVir
+gatunki_liczebność.loc['Razem'] = liczebnoscCala
 
 liczności = pd.DataFrame()
 liczności['Liczebność'] = gatunki_liczebność
@@ -22,27 +45,27 @@ liczności.index.name = 'Gatunek'
 pd.set_option('display.float_format', '{:.1f}'.format)
 print('Liczności poszczególnych gatunków:')
 print(liczności.to_string(index=True, header=True, justify='left'))
-
+print(" ")
 
 # Miary rozkładu każdej cechy (miary położenia rozkładu)
 
 charakterystyka = pd.DataFrame(columns=['Minimum', 'Śr. arytm.', '± Odch. stand.', 'Q1', 'Q3', 'Mediana', 'Maksimum'])
 
-charakterystyka.loc["Długość działki kielicha (cm)", "Minimum"] = (dane['Sepal length']).min()
-charakterystyka.loc["Długość działki kielicha (cm)", "Śr. arytm."] = (dane['Sepal length']).mean()
-charakterystyka.loc["Długość działki kielicha (cm)", "± Odch. stand."] = (dane['Sepal length']).std()
+charakterystyka.loc["Długość działki kielicha (cm)", "Minimum"] = dlKielicha.min()
+charakterystyka.loc["Długość działki kielicha (cm)", "Śr. arytm."] = dlKielicha.mean()
+charakterystyka.loc["Długość działki kielicha (cm)", "± Odch. stand."] = dlKielicha.std()
 charakterystyka.loc["Długość działki kielicha (cm)", "Q1"] = (dane['Sepal length']).quantile(0.25)
 charakterystyka.loc["Długość działki kielicha (cm)", "Q3"] = (dane['Sepal length']).quantile(0.75)
-charakterystyka.loc["Długość działki kielicha (cm)", "Mediana"] = (dane['Sepal length']).median()
-charakterystyka.loc["Długość działki kielicha (cm)", "Maksimum"] = (dane['Sepal length']).max()
+charakterystyka.loc["Długość działki kielicha (cm)", "Mediana"] = np.median(dlKielicha)
+charakterystyka.loc["Długość działki kielicha (cm)", "Maksimum"] = dlKielicha.max()
 
-charakterystyka.loc["Szerokość działki kielicha (cm)", "Minimum"] = (dane['Sepal width']).min()
-charakterystyka.loc["Szerokość działki kielicha (cm)", "Śr. arytm."] = (dane['Sepal width']).mean()
-charakterystyka.loc["Szerokość działki kielicha (cm)", "± Odch. stand."] = (dane['Sepal width']).std()
+charakterystyka.loc["Szerokość działki kielicha (cm)", "Minimum"] = szerKielicha.min()
+charakterystyka.loc["Szerokość działki kielicha (cm)", "Śr. arytm."] = szerKielicha.mean()
+charakterystyka.loc["Szerokość działki kielicha (cm)", "± Odch. stand."] = szerKielicha.std()
 charakterystyka.loc["Szerokość działki kielicha (cm)", "Q1"] = (dane['Sepal width']).quantile(0.25)
 charakterystyka.loc["Szerokość działki kielicha (cm)", "Q3"] = (dane['Sepal width']).quantile(0.75)
-charakterystyka.loc["Szerokość działki kielicha (cm)", "Mediana"] = (dane['Sepal width']).median()
-charakterystyka.loc["Szerokość działki kielicha (cm)", "Maksimum"] = (dane['Sepal width']).max()
+charakterystyka.loc["Szerokość działki kielicha (cm)", "Mediana"] = np.median(szerKielicha)
+charakterystyka.loc["Szerokość działki kielicha (cm)", "Maksimum"] = szerKielicha.max()
 
 pd.set_option('display.float_format', '{:.2f}'.format)
 print('Charakterystyka cech irysów:')
