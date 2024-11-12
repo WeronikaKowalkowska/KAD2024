b749f5c847d49a7a35693d48f247634ed3cdb784
Nadzeya Silchankava <nadezhdasilchenkova@gmail.com>
2024-11-12 09:51:28 +0100

    data file name

--- Zadanie 1/main.py
+++ Zadanie 1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dane = pd.read_csv('dane.csv',
+dane = pd.read_csv('data1.csv',
                    header = None,
                    names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Species'])
 
